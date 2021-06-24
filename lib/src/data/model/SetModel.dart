9d19120e16055d64db939051561a700d0745e1bb
kacpercwiklinski <kacper.cwiklinski9@gmail.com>
2021-06-24 19:01:21 +0200

    Database diagram

---
+++ lib/src/data/model/SetModel.dart
@@ -0,0 +1,28 @@
+import 'package:gym_tracker/src/data/model/BaseModel.dart';
+import 'package:gym_tracker/src/data/model/MuscleGroupModel.dart';
+
+class ExerciseModel implements BaseModel {
+  int id;
+  String name;
+  MuscleGroupModel muscleGroup;
+
+  ExerciseModel();
+  ExerciseModel.name(this.name);
+  ExerciseModel.allArgs(this.id, this.name, this.muscleGroup);
+
+  @override
+  fromMap(Map<String, dynamic> data) {
+    MuscleGroupModel muscleGroupModel = MuscleGroupModel.allArgs(
+        data['muscle_group_id'], data['muscle_group_name']);
+    return ExerciseModel.allArgs(data['id'], data['name'], muscleGroupModel);
+  }
+
+  Map<String, dynamic> toMap() {
+    return {'id': id, 'name': name, 'muscle_group_id': muscleGroup.id};
+  }
+
+  @override
+  String toString() {
+    return "id = $id, name = $name, muscle_group_id = ${muscleGroup.id}";
+  }
+}
